abb1b2fafd9915798651527510895b51eba1ab96
Kevin Martinez <kevinprincipal9@gmail.com>
2022-06-24 09:54:00 -0500

    chore: Added player mobility functions
    Added gotoxy() function to the code
    Added Ocultar_el_cursor() to the code to hide the cursor from the screen
    Asigned the movement of the avatar to the constants of arrow keys
    
    @Al3xMR - EPN

--- CofigoFuente.cpp
+++ CofigoFuente.cpp
@@ -1,3 +1,4 @@
+
 //LABERINTO
 //ESCUELA POLITECNICA NACIONAL | PROGRAMACION I, CD | 22/02/2022
 //HECHO POR: MARTINEZ KEVIN, FERNANDEZ ANDRES, ENRIQUEZ MELANY.
@@ -7,11 +8,67 @@
 #include <string>
 using namespace std;
 using namespace conio;
+
+
+
+
 #define ARRIBA 72
 #define IZQUIERDA 75
 #define DERECHA 77
 #define ABAJO 80
+
+
+
+void gotoxy (int x, int y){
+	HANDLE hCon;
+	hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD dwPos;
+	dwPos.X = x;
+	dwPos.Y = y;
+	
+	SetConsoleCursorPosition(hCon, dwPos);
+}
+
+
+void Ocultar_el_Cursor (){
+	HANDLE hCon;
+	hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO cci;
+	cci.dwSize = 10	;
+	cci.bVisible = FALSE;
+	
+	SetConsoleCursorInfo(hCon, &cci);
+}
+
+
 int main(){
 	
+	Ocultar_el_Cursor();
+	int x = 10, y = 10;
+	gotoxy (x,y); 
+	printf("%c", 5);
+	
+	
+	bool game_over = false;
+	while (!game_over){
+		
+		if (kbhit()){
+			char tecla = getch();
+			gotoxy (x,y); 
+			cout << (" ");
+			if(tecla == IZQUIERDA) x--;
+			if(tecla == DERECHA) x++;
+			if(tecla == ARRIBA) y--;
+			if(tecla == ABAJO) y++;
+			gotoxy (x,y); 
+			printf("%c", 5);
+		}
+		
+		Sleep (30);		
+	}
 	return 0;
 }
+
+
+
+
