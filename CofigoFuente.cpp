56f0d6e7182a25e7a4f6a84391f6dfce2ad26e94
Kevin Martinez <kevinprincipal9@gmail.com>
2022-06-24 09:45:46 -0500

    chore: Added first libraries to the project
    Added stdio.h, constream, windows.h and string libraries to the project
    Define constants to asign a name to arrow keys number
    
    @Al3xMR - EPN

---
+++ CofigoFuente.cpp
@@ -0,0 +1,17 @@
+//LABERINTO
+//ESCUELA POLITECNICA NACIONAL | PROGRAMACION I, CD | 22/02/2022
+//HECHO POR: MARTINEZ KEVIN, FERNANDEZ ANDRES, ENRIQUEZ MELANY.
+#include <stdio.h>
+#include <constream>
+#include <windows.h>
+#include <string>
+using namespace std;
+using namespace conio;
+#define ARRIBA 72
+#define IZQUIERDA 75
+#define DERECHA 77
+#define ABAJO 80
+int main(){
+	
+	return 0;
+}
